7913dd6f3e3ed869f7bc61e4a5287e8c34f813d1
Kalil <kalilgarcia38@gmail.com>
2024-08-06 14:18:20 -0300

    inserção de testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,24 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # deu certo? ou '==200'assert
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_read_page_must_return_html():
+    client = TestClient(app)
+
+    response = client.get('/ola')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>
+"""
+    )

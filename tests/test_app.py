f45b6b981f0813218f70ba433875200ca6a8a0a0
Kalil <kalilgarcia38@gmail.com>
2024-08-09 18:40:08 -0300

    Atualizando endpoints pra usar o db real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
 
+'''
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     # client = TestClient(app)  --- arrange
 
@@ -28,17 +30,18 @@ def test_read_page_must_return_html(client):
 """
     )
 
+'''
 
-def test_create_user_must_return_created(client):
+
+def test_create_user(client):
     response = client.post(
-        '/users/',
+        '/users',
         json={
             'username': 'alice',
             'email': 'alice@example.com',
             'password': 'secret',
         },
     )
-
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
         'username': 'alice',
@@ -48,20 +51,19 @@ def test_create_user_must_return_created(client):
 
 
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get('/users')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+# teste para ver se o endpoint retorna o usuário correto quando existe um usuário no banco
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -78,7 +80,7 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
